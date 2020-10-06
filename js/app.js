2832ae6c4d3403de1ce085669753051d592d1358
Eduardo <thiago.novak7@gmail.com>
2020-10-06 10:09:14 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+
+const elementosBtns = document.querySelectorAll('.menu__wars span');
+
+elementosBtns.forEach((elementoBtn) => {
+
+    elementoBtn.addEventListener('click', filtro)
+
+
+
+
+})
+
+
+function filtro(){
+    console.log('item');
+}
