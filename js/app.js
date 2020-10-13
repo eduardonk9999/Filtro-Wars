f6cf4e51e0ee6a9be5d4f6f16299041d31d8ca90
Eduardo <thiago.novak7@gmail.com>
2020-10-12 22:52:40 -0300

    Add, filter aplicado

--- js/app.js
+++ js/app.js
@@ -14,13 +14,21 @@ function clickBtn() {
         btn.addEventListener('click', () => {
             if (btn.dataset.item === 'nave') {
                 console.log('nave')
-                
-                // Ver aula ori 0407
-
-
-                filterElements();
+                filterElements(btn.dataset.item);
             } else {
                 console.log('nao sou nave')
+            }
+            if (btn.dataset.item === 'jedi') {
+                console.log('jedi')
+                filterElements(btn.dataset.item);
+            } else {
+                console.log('nao sou jedi')
+            }
+            if (btn.dataset.item === 'piloto') {
+                console.log('piloto')
+                filterElements(btn.dataset.item);
+            } else {
+                console.log('nao sou piloto')
             }
         })
     }
@@ -30,17 +38,34 @@ function clickBtn() {
 
 // Filter Elements
 
-function filterElements() {
-    // const itensNave = groupWars.map((item) => {
-    //     return item;
-    // });
+function filterElements(par) {
+    
 
     const itensNave = groupWars.filter((item) => {
-        console.log(item.name)
-        return item.name === 'nave';
+        if(item.name === `${par}`){
+            console.log(item.name);
+
+            const divsUnico = document.querySelectorAll('.personas__wars div');
+
+            for (const divUnico of divsUnico) {
+                if(divUnico.getAttribute('data-filterwars') === `${par}`){
+                    divUnico.classList.add('active');
+                }
+                else{
+                    divUnico.classList.remove('active');
+                }
+                
+
+                
+            }
+            
+        }
+        
     })
 
-    console.log(itensNave);
+    console.log(itensNave); // array vazio
+
+   
     
 }
 
